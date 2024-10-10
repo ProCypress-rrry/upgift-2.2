3b18896561cb2cc2594c2f8e09a6a7a935489b2f
Victor Johansson <vijo07001@utb.vaxjo.se>
2024-10-10 12:20:52 +0200

    Add project files.

---
+++ upgift 2.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Upgift22
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("hur långt hoppade Elin i meter? (inga bokstäver)");
+            string elinlängd = Console.ReadLine();
+            int eLängd = int.Parse(elinlängd);
+            Console.WriteLine("hur långt hoppade Alma? (inga bokstäver)");
+            string Almalängd = Console.ReadLine();
+            int Alängd = int.Parse(Almalängd);
+            int X = eLängd - Alängd;
+            Console.WriteLine("Elin hoppade" + " " + X + " " + "Meter längre än Alma");
+        } 
+    }   
+}
